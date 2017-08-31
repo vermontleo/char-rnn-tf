ccfcf51f3898c3cc226bea62b7aac98dffde32c0
vermontleo <2317611168@qq.com>
2017-08-31 22:08:18 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,22 +1,23 @@
 #coding:utf-8
 
 class Config(object):
-    init_scale = 0.04
-    learning_rate = 0.001
-    max_grad_norm = 15
-    num_layers = 3
-    num_steps = 30 # number of steps to unroll the RNN for
-    hidden_size = 800 # size of hidden layer of neurons
-    iteration = 30
-    save_freq = 5 #The step (counted by the number of iterations) at which the model is saved to hard disk.
-    keep_prob = 0.5
-    batch_size = 128
-    model_path = './Model' #the path of model that need to save or load
+    init_scale = 0.04 #参数是用均匀分布进行初始化，该值为均匀分布的上下界
+    learning_rate = 0.001 #学习率
+    max_grad_norm = 15 #对梯度进行规划化（gradient clipping）
+    num_layers = 3 #RNN的层级数
+    num_steps = 15 # RNN展开的步骤数（每次训练多少个字符）
+    hidden_size = 800 # 神经网络隐含层的维度
+    iteration = 100 #模型总共迭代次数
+    save_freq = 10 #每迭代多少次保存一次模型，同时进行一次生成
+    keep_prob = 0.5 #dropout的概率
+    batch_size = 128 #min-batch的大小
+    model_path = './model/Model' #模型保存的路径
     
     #parameters for generation
-    save_time = 20 #load save_time saved models
-    is_sample = True #true means using sample, if not using max
-    is_beams = True #whether or not using beam search
-    beam_size = 2 #size of beam search
-    len_of_generation = 100 #The number of characters by generated
-    start_sentence = u'那是因为我看到了另一个自己的悲伤' #the seed sentence to generate text
+    save_time = 90 #载入第save_time次保存的模型
+    is_sample = True #是否采用sample策略，设置为False是采用max策略
+    is_beams = True #是否采用beam-search进行解码，设置为False时不采用（相当于beam_size=1）
+    beam_size = 2 #beam-search的窗口大小
+    len_of_generation = 3500 #期望生成文本的长度（包括多少个字）
+    gen_path = './output/output.txt' #生成文本的保存路径
+    start_sentence = u'他回头看' #期望生成文本的开始部分（可以是一个句子，一个词，也可以仅仅只是一个字）
